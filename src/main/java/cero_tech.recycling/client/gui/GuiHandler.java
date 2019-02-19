abdd9503987e9d7eed8296f7c20797c6775c42bc
Andrew Brooking <andrew.brooking@gmail.com>
2019-02-19 14:54:01 -0600

    Finished implementing GUI and rendering for Shredder

---
+++ src/main/java/cero_tech.recycling/client/gui/GuiHandler.java
@@ -0,0 +1,53 @@
+package cero_tech.recycling.client.gui;
+
+import cero_tech.recycling.client.containers.ContainerShredder;
+import cero_tech.recycling.common.tileentities.TileEntityShredder;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.IGuiHandler;
+
+import javax.annotation.Nullable;
+
+/**
+ * Name: GuiHandler
+ * Description: Handles GUI/Container implementation
+ * Author: cero_tech
+ * <p>
+ * Last Update: 2/19/2019
+ **/
+
+public class GuiHandler implements IGuiHandler {
+
+    public static final int GUI_SHREDDER_ID = 0;
+    public static final int GUI_EXCHANGER_ID = 1;
+
+    @Nullable
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        switch (ID) {
+            case GUI_SHREDDER_ID:
+                TileEntity te = world.getTileEntity(new BlockPos(x, y, z));
+                return new ContainerShredder(player.inventory, (TileEntityShredder) te);
+            case GUI_EXCHANGER_ID:
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        switch (ID) {
+            case GUI_SHREDDER_ID:
+                TileEntity te = world.getTileEntity(new BlockPos(x, y, z));
+                return new GuiShredder(player.inventory, (TileEntityShredder) te);
+            case GUI_EXCHANGER_ID:
+                return null;
+            default:
+                return null;
+        }
+    }
+}
---
+++ src/main/java/cero_tech.recycling/common/config/ConfigGeneral.java
@@ -0,0 +1,28 @@
+package cero_tech.recycling.common.config;
+
+import cero_tech.recycling.Recycling;
+import net.minecraftforge.common.config.Config;
+
+/**
+ * Name: ConfigGeneral
+ * Description: Contains all general configuration variable.
+ * Author: cero_tech
+ * <p>
+ * Last Update: 2/19/2019
+ **/
+
+@Config(modid = Recycling.MOD_ID, name = Recycling.MOD_NAME)
+public class ConfigGeneral {
+
+    @Config.Name("Max Stack Size")
+    @Config.RangeInt(min = 1)
+    public static final int maxStackSize = 64;
+
+    @Config.Name("Machine Energy Capacity")
+    @Config.RangeInt(min = 1)
+    public static final int energyCapacity = 10000;
+
+    @Config.Name("Machine Energy Usage")
+    @Config.RangeInt(min = 0)
+    public static final int energyUsage = 100;
+}
